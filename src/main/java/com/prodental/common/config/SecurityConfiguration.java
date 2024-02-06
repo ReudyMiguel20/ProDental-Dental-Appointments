8ea32fbf5e9ab4dc6cf7db1f9e2c07e69082f9f8
Reudy Guerrero <87297026+ReudyMiguel20@users.noreply.github.com>
2024-02-06 11:23:14 -0400

    User Authentication

--- src/main/java/com/prodental/common/config/SecurityConfiguration.java
+++ src/main/java/com/prodental/common/config/SecurityConfiguration.java
@@ -1,18 +1,25 @@
 package com.prodental.common.config;
 
+import com.prodental.common.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,9 +28,18 @@ public class SecurityConfiguration {
 
                 .authorizeHttpRequests(auth -> {
                     auth
+                            .requestMatchers("/api/v1/user/testadmin").hasAnyAuthority("ADMIN")
+                            .requestMatchers("/api/v1/user/testadmin").hasAnyRole( "ADMIN")
+                            .requestMatchers("/api/v1/user/testuser").hasAnyAuthority("USER")
+                            .requestMatchers("/api/v1/user/testuser").hasAnyRole( "USER")
                             .anyRequest()
                             .permitAll();
                 })
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
 
                 .cors(AbstractHttpConfigurer::disable)
                 .headers(AbstractHttpConfigurer::disable)
--- src/main/java/com/prodental/user/controller/UserController.java
+++ src/main/java/com/prodental/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.prodental.user.controller;
 import com.prodental.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,19 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/test")
-    public ResponseEntity<String> test() {
+    public ResponseEntity<String> test(Authentication auth) {
         return ResponseEntity.ok("Test");
     }
+
+    @GetMapping("/testadmin")
+    public ResponseEntity<String> testAdmin(Authentication auth) {
+        return ResponseEntity.ok("Test");
+    }
+
+    @GetMapping("/testuser")
+    public ResponseEntity<String> testUser(Authentication auth) {
+        return ResponseEntity.ok("Test");
+    }
+
+
 }
