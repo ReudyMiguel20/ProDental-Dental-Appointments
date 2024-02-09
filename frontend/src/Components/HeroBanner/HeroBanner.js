3863a4afc1b6a682c509bd07878ebddb4b8b3984
Reudy Guerrero <87297026+ReudyMiguel20@users.noreply.github.com>
2024-02-09 11:53:57 -0400

    Styling HeroBanner

--- frontend/src/Components/HeroBanner/HeroBanner.js
+++ frontend/src/Components/HeroBanner/HeroBanner.js
@@ -6,6 +6,20 @@ const HeroBanner = () => {
   return (
     <div className="hero-banner">
       <img className="hero-banner-img" src={herobanner} alt="hero-banner" />
+
+      <div className="hero-banner-text">
+        <span className="hero-banner-text-one">Nosotros Cuidaremos</span>
+        <span className="hero-banner-text-two">Tus Dientes</span>
+
+        <div className="hero-banner-description">
+          <span>Pro Dental ofrece una gama completa de servicios dentales</span>
+          <span>tanto para adultos como niños.</span>
+        </div>
+
+        <div className="hero-appointment">
+          <button>Agenda Tu Cita</button>
+        </div>
+      </div>
     </div>
   );
 };
