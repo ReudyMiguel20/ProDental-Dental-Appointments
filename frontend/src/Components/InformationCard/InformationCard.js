3593fdd3aff8e608daa8cf3dba3ce3e40675cf9f
Reudy Guerrero <87297026+ReudyMiguel20@users.noreply.github.com>
2024-02-09 16:20:39 -0400

    Imported FontAwesomeIcons

--- frontend/src/Components/InformationCard/InformationCard.js
+++ frontend/src/Components/InformationCard/InformationCard.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./InformationCard.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarCheck } from '@fortawesome/free-solid-svg-icons'
 
 
 const InformationCard = () => {
   return (
     <div className="information-card-container">
       <div className="information-card-main">
+        <div>
         <div className="information-card-icon">
-          <img src={} alt=""/>
-          Icon Here
+        <FontAwesomeIcon icon={faCalendarCheck} size='3x' />
         </div>
 
         <div className="information-card-title">
