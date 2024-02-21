e576ebc9a448681ac148a6111c55b113d035eb22
Reudy Guerrero <87297026+ReudyMiguel20@users.noreply.github.com>
2024-02-21 15:10:11 -0400

    Added dashboard content

--- frontend/src/Pages/Dashboard/UserDashboard/UserDashboard.js
+++ frontend/src/Pages/Dashboard/UserDashboard/UserDashboard.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import "./UserDashboard.css"
 import Sidebar from "../../../Components/Sidebar/Sidebar";
-import { userItems } from "../../../Data/UserSidebarData";
+import {userItems} from "../../../Data/UserSidebarData";
+import {Route, Routes} from "react-router-dom";
+import Home from "../../Home";
+import Contact from "../../Contact";
 
 const UserDashboard = () => {
-  return (
-    <div className="user-dashboard-container">
-        <Sidebar items={userItems}/>
-    </div>
-  )
+    return (
+        <div className="user-dashboard-container">
+            <Sidebar items={userItems}/>
+
+            <div className="dashboard-content">
+                <Routes>
+                    <Route path="/" element={<Contact/>}/>
+                </Routes>
+            </div>
+        </div>
+    )
 }
 
 export default UserDashboard
