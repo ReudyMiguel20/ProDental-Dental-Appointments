cae9fdaf5d8051899c94bbca28365c000e14ee66
Reudy Guerrero <87297026+ReudyMiguel20@users.noreply.github.com>
2024-02-28 19:02:59 -0400

    Update CurrentAppointments.js

--- frontend/src/Components/CurrentAppointments/CurrentAppointments.js
+++ frontend/src/Components/CurrentAppointments/CurrentAppointments.js
@@ -4,40 +4,7 @@ import Table from 'react-bootstrap/Table';
 const CurrentAppointments = () => {
   return (
     <div>
-        {/*<Table striped bordered hover>*/}
-        {/*    <thead>*/}
-        {/*    <tr>*/}
-        {/*        <th>#</th>*/}
-        {/*        <th>Estado</th>*/}
-        {/*        <th>Fecha</th>*/}
-        {/*        <th>Hora</th>*/}
-        {/*        <th>Procedimiento</th>*/}
-        {/*        <th>Dentista</th>*/}
-        {/*        <th>Lugar</th>*/}
-        {/*    </tr>*/}
-        {/*    </thead>*/}
-        {/*    <tbody>*/}
-        {/*    <tr>*/}
-        {/*        <td>1</td>*/}
-        {/*        <td>Mark</td>*/}
-        {/*        <td>Otto</td>*/}
-        {/*        <td>@mdo</td>*/}
-        {/*    </tr>*/}
-        {/*    <tr>*/}
-        {/*        <td>2</td>*/}
-        {/*        <td>Jacob</td>*/}
-        {/*        <td>Thornton</td>*/}
-        {/*        <td>@fat</td>*/}
-        {/*    </tr>*/}
-        {/*    <tr>*/}
-        {/*        <td>3</td>*/}
-        {/*        <td>Jacob</td>*/}
-        {/*        <td>Thornton</td>*/}
-        {/*        <td>@fat</td>*/}
-        {/*    </tr>*/}
-        {/*    </tbody>*/}
-        {/*</Table>*/}
-
+        //
         <Table responsive>
             <thead>
             <tr>
