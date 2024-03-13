e3bdadddff516249be87a8386630f6c9d555a514
Reudy Guerrero <87297026+ReudyMiguel20@users.noreply.github.com>
2024-03-12 21:11:53 -0300

    Update AdminUserManagement.js

--- frontend/src/Components/AdminUserManagement/AdminUserManagement.js
+++ frontend/src/Components/AdminUserManagement/AdminUserManagement.js
@@ -15,7 +15,7 @@ const AdminUserManagement = () => {
   // Functions using the API
 
   const fetchUsers = async () => {
-    const response = await fetch("http://prodental-website-df2c2d1fd4bd.herokuapp.com/api/v1/admin/users", {
+    const response = await fetch("https://prodental-website-df2c2d1fd4bd.herokuapp.com/api/v1/admin/users", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
